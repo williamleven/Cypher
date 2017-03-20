28bef43c3269c03666a41a6339c01e6ad256bbe3
Gurgy <Gurgy@emycia.com>
2017-03-20 23:07:29 +0100

    Setup maven

---
+++ src/main/java/edu/chalmers/tda367/App.java
@@ -0,0 +1,13 @@
+package edu.chalmers.tda367;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
