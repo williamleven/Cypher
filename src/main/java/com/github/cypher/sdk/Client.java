a68e67896601cfe417d36d6595d4937dd063f2b8
hulthe <joakim@hulthe.net>
2017-04-03 15:53:27 +0200

    Implement /login & /sync api endpoints

--- src/main/java/com/github/cypher/sdk/Client.java
+++ src/main/java/com/github/cypher/sdk/Client.java
@@ -1,5 +1,15 @@
 package com.github.cypher.sdk;
 
+import com.github.cypher.sdk.api.ApiLayer;
+
+import java.io.IOException;
+
 // Poses as a chat instance, being the object returned from this package
 public class Client {
+
+    private ApiLayer api;
+
+    public Client(ApiLayer api) throws IOException {
+        this.api = api;
+    }
 }
