46fb15bfec2eb5e9b1bc1e6c45fcb7e11a85be9e
Feffe <c.fredrik.aberg@gmail.com>
2017-04-03 15:35:42 +0200

    Added Member class.

--- src/main/java/com/github/cypher/sdk/Member.java
+++ src/main/java/com/github/cypher/sdk/Member.java
@@ -2,4 +2,31 @@ package com.github.cypher.sdk;
 
 // Represents a member of a room
 public class Member {
+    private final User user;
+    private final String displayname;
+    private final String avatarUrl;
+    private final String membership;
+
+    public Member(User user, String displayname, String avatarUrl, String membership) {
+        this.user = user;
+        this.displayname = displayname;
+        this.avatarUrl = avatarUrl;
+        this.membership = membership;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public User getUser() {
+        return new User(user);
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public String getMembership() {
+        return membership;
+    }
 }
