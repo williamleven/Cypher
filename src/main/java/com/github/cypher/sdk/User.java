4db8246733b2a71f9aeb684ee13520bc4769a154
Feffe <c.fredrik.aberg@gmail.com>
2017-04-03 16:12:34 +0200

    Add User class.

--- src/main/java/com/github/cypher/sdk/User.java
+++ src/main/java/com/github/cypher/sdk/User.java
@@ -2,4 +2,49 @@ package com.github.cypher.sdk;
 
 // Represents a user
 public class User {
+    protected String id;
+    protected String name;
+    protected String url;
+    protected boolean isActive;
+    protected long lastActiveAgo;
+
+
+    public User(User user) {
+        id = user.getId();
+        name = user.getName();
+        url = user.getURl();
+        isActive = user.getIsActive();
+        lastActiveAgo = user.getLastActiveAgo();
+
+    }
+
+    public void update(){
+        //TODO
+    }
+
+
+
+
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getURl() {
+        return url;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public long getLastActiveAgo() {
+        return lastActiveAgo;
+    }
 }
