30ea65cd83f6831a3cdf0d9bf458e85e5b4ea2bf
Feffe <c.fredrik.aberg@gmail.com>
2017-04-05 14:15:32 +0200

    Tabify Member and User classes.

--- src/main/java/com/github/cypher/sdk/Member.java
+++ src/main/java/com/github/cypher/sdk/Member.java
@@ -2,22 +2,21 @@ package com.github.cypher.sdk;
 
 // Represents a member of a room
 public class Member {
-    private final User user;
-    private final int privilege;
+	private final User user;
+	private final int privilege;
 
-    public Member(User user, int privilege) {
-        this.user = user;
-        this.privilege = privilege;
-    }
-
-
-
-    public User getUser() {
-        return user;
-    }
+	public Member(User user, int privilege) {
+		this.user = user;
+		this.privilege = privilege;
+	}
 
 
-    public int getPrivilege() {
-        return privilege;
-    }
+	public User getUser() {
+		return user;
+	}
+
+
+	public int getPrivilege() {
+		return privilege;
+	}
 }
