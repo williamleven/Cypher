9cef22e9e86acac095abe305bca7348b3664edbf
Gurgy <Gurgy@emycia.com>
2017-04-07 11:56:47 +0200

    Create a Session class

--- src/main/java/com/github/cypher/sdk/api/MatrixApiLayer.java
+++ src/main/java/com/github/cypher/sdk/api/MatrixApiLayer.java
@@ -1,6 +1,5 @@
 package com.github.cypher.sdk.api;
 
-import com.github.cypher.DebugLogger;
 import com.github.cypher.sdk.User;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -14,34 +13,34 @@ import java.util.Map;
  * This class provides access to matrix endpoints trough
  * ordinary java methods returning Json Objects.
  *
- * <p>Most endpoints require the {@link #login(String, String, String)} method to
- * have been successfully run as the data provided by the
+ * <p>Most endpoints require a session to
+ * have been successfully initiated as the data provided by the
  * endpoint itself isn't publicly available.
+ *
+ * <p>A session can be initiated with {@link #login(String, String, String)}
+ * or wia the constructor {@link #MatrixApiLayer(String, String, String)}
  */
 public class MatrixApiLayer implements ApiLayer {
 
-	private String userId;
-	private String accessToken;
-	private String homeServer;
-	private String deviceId;
+	private Session session;
 
-	public MatrixApiLayer(String username, String password, String homeserver) throws IOException {
-
-		JsonObject response = login(username, password, homeserver);
-
-		if(response.has("errcode")) {
-			if(DebugLogger.ENABLED) {
-				DebugLogger.log(response.get("error").getAsString());
-				DebugLogger.log(response.get("errcode").getAsString());
-			}
-			return;
-		}
-
-		this.userId = response.get("user_id").getAsString();
-		this.accessToken = response.get("access_token").getAsString();
-		this.homeServer = response.get("home_server").getAsString();
-		this.deviceId = response.get("device_id").getAsString();
+	/**
+	 * Creates a MatrixApiLayer with a session.
+	 *
+	 * @param username Username
+	 * @param password Password
+	 * @param homeserver A homeserver to connect trough
+	 */
+	public MatrixApiLayer(String username, String password, String homeserver) throws ExtendedHTTPException, IOException {
+		login(username, password, homeserver);
 	}
+
+	/**
+	 * Crates a new MatrixApiLayer without a session.
+	 *
+	 * <p> Use {@link #login(String, String, String)} to create a session.
+	 */
+	public MatrixApiLayer() {}
 
 	/**
 	 * Authenticates the user.
@@ -49,10 +48,14 @@ public class MatrixApiLayer implements ApiLayer {
 	 * @param username Username
 	 * @param password Password
 	 * @param homeserver A homeserver to connect trough
-	 * @return Valid Json response
 	 */
 	@Override
-	public JsonObject login(String username, String password, String homeserver) throws ExtendedHTTPException, IOException {
+	public void login(String username, String password, String homeserver) throws ExtendedHTTPException, IOException {
+		// Only run if session isn't already set
+		if (session != null){
+			return;
+		}
+
 		URL url = Util.UrlBuilder(homeserver, Endpoint.LOGIN, null);
 
 		JsonObject request  = new JsonObject();
@@ -60,7 +63,9 @@ public class MatrixApiLayer implements ApiLayer {
 		request.addProperty("user", username);
 		request.addProperty("password", password);
 
-		return Util.makeJsonPostRequest(url, request).getAsJsonObject();
+		JsonObject response = Util.makeJsonPostRequest(url, request).getAsJsonObject();
+
+		this.session = new Session(response);
 	}
 
 	/**
@@ -80,9 +85,9 @@ public class MatrixApiLayer implements ApiLayer {
 		if(since  != null && !since.equals(""))  parameters.put("since"       , since);
 		if(setPresence != null)                 parameters.put("set_presence", setPresence.name());
 		parameters.put("full_state"  , fullState ? "true" : "false");
-		parameters.put("access_token", accessToken);
+		parameters.put("access_token", session.getAccessToken());
 
-		URL url = Util.UrlBuilder(homeServer, Endpoint.SYNC, parameters);
+		URL url = Util.UrlBuilder(session.getHomeServer(), Endpoint.SYNC, parameters);
 
 		JsonElement response = Util.makeJsonGetRequest(url);
 		return response.getAsJsonObject();
