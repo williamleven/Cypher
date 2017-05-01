c3ac016c352cc70181ce0aa9deb0f8166f9e948e
NotTrulyEvil <ludvig.lyl@gmail.com>
2017-05-01 21:23:16 +0200

    Create USER_DATA_DIRECTORY constant in Main

--- src/main/java/com/github/cypher/TOMLSettings.java
+++ src/main/java/com/github/cypher/TOMLSettings.java
@@ -3,18 +3,16 @@ package com.github.cypher;
 import com.moandjiezana.toml.Toml;
 import com.moandjiezana.toml.TomlWriter;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
+
+import static com.github.cypher.Main.USER_DATA_DIRECTORY;
 
 public class TOMLSettings implements Settings {
 
 	// Application specific constants
-	private static final String APPLICATION_NAME = "cypher";
 	private static final String FILE_NAME = "config.toml";
-
-	// System specific constants
-	private static final String APPDATA_DIRECTORY = System.getenv("APPDATA");
-	private static final String HOME_DIRECTORY = System.getProperty("user.home");
 
 	// Instance variables
 	private final File settingsFile;
@@ -34,10 +32,10 @@ public class TOMLSettings implements Settings {
 	private static File createOrLoadFile(){
 		try {
 			// Create folder if it doesn't exist
-			new File(settingsFolderPath()).mkdir();
+			new File(USER_DATA_DIRECTORY).mkdir();
 
 			// Load File
-			File file = new File(settingsFolderPath() + File.separator + FILE_NAME);
+			File file = new File(USER_DATA_DIRECTORY + File.separator + FILE_NAME);
 
 			// Create file if it doesn't exist
 			file.createNewFile();
@@ -92,22 +90,5 @@ public class TOMLSettings implements Settings {
 	public synchronized void setLanguage(Locale language) {
 		settingsData.languageTag = language.toLanguageTag();
 		save();
-	}
-
-	// Creates the settings folder path
-	private static String settingsFolderPath(){
-		if ( APPDATA_DIRECTORY != null){ // Windows style
-			return APPDATA_DIRECTORY + File.separator + capitalize(APPLICATION_NAME);
-		}else{ //Unix style
-			return HOME_DIRECTORY + File.separator + "." + decapitalize(APPLICATION_NAME);
-		}
-	}
-
-	// Utility methods
-	private static String capitalize(String name){
-		return name.substring(0, 1).toUpperCase() + name.substring(1);
-	}
-	private static String decapitalize(String name){
-		return name.substring(0, 1).toLowerCase() + name.substring(1);
 	}
 }
