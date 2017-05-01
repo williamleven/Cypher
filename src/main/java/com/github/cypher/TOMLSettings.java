3b441f3f48a34ec2309e1a4a3242cc40e6b51998
Gurgy <Gurgy@emycia.com>
2017-05-01 12:05:52 +0200

    Correct save path for Windows

--- src/main/java/com/github/cypher/TOMLSettings.java
+++ src/main/java/com/github/cypher/TOMLSettings.java
@@ -8,14 +8,19 @@ import java.util.Locale;
 
 public class TOMLSettings implements Settings {
 
-	private static final String HOME_DIRECTORY = System.getProperty("user.home");
-
+	// Application specific constants
+	private static final String APPLICATION_NAME = "cypher";
 	private static final String FILE_NAME = "config.toml";
 
-	private final File settingsFile;
+	// System specific constants
+	private static final String APPDATA_DIRECTORY = System.getenv("APPDATA");
+	private static final String HOME_DIRECTORY = System.getProperty("user.home");
 
+	// Instance variables
+	private final File settingsFile;
 	private final SettingsData settingsData;
 
+	// Class representing all settings
 	private static class SettingsData{
 		String languageTag = Locale.getDefault().toLanguageTag(); // Default Value
 	}
@@ -28,31 +33,21 @@ public class TOMLSettings implements Settings {
 
 	private static File createOrLoadFile(){
 		try {
-
 			// Create folder if it doesn't exist
-			new File(HOME_DIRECTORY + File.separator + ".cypher").mkdir();
+			new File(settingsFolderPath()).mkdir();
 
 			// Load File
-			File file = new File(HOME_DIRECTORY + File.separator + ".cypher" + File.separator + FILE_NAME);
+			File file = new File(settingsFolderPath() + File.separator + FILE_NAME);
 
 			// Create file if it doesn't exist
 			file.createNewFile();
 
 			return file;
+
 		}catch (IOException e) {
 			DebugLogger.log("Could not create settings file");
 			return null;
 		}
-	}
-
-
-	public synchronized Locale getLanguage() {
-		return Locale.forLanguageTag(settingsData.languageTag);
-	}
-
-	public synchronized void setLanguage(Locale language) {
-		settingsData.languageTag = language.toLanguageTag();
-		save();
 	}
 
 	private static synchronized SettingsData load(File settingsFile) {
@@ -80,4 +75,29 @@ public class TOMLSettings implements Settings {
 		}
 	}
 
+	// Language setting
+	public synchronized Locale getLanguage() {
+		return Locale.forLanguageTag(settingsData.languageTag);
+	}
+	public synchronized void setLanguage(Locale language) {
+		settingsData.languageTag = language.toLanguageTag();
+		save();
+	}
+
+	// Creates the settings folder path
+	private static String settingsFolderPath(){
+		if ( APPDATA_DIRECTORY != null){ // Windows style
+			return APPDATA_DIRECTORY + File.separator + capitalize(APPLICATION_NAME);
+		}else{ //Unix style
+			return HOME_DIRECTORY + File.separator + "." + decapitalize(APPLICATION_NAME);
+		}
+	}
+
+	// Utility methods
+	private static String capitalize(String name){
+		return name.substring(0, 1).toUpperCase() + name.substring(1);
+	}
+	private static String decapitalize(String name){
+		return name.substring(0, 1).toLowerCase() + name.substring(1);
+	}
 }
