5fae057ad5d3a7062bf9dffbd5278544fc0f53c1
Gurgy <Gurgy@emycia.com>
2017-05-02 09:34:24 +0200

    Add code for image conversion (reverted from commit 20169aff086f22175c6b7a78a680de2b1c7bdf3a)

--- src/main/java/com/github/cypher/model/Util.java
+++ src/main/java/com/github/cypher/model/Util.java
@@ -1,13 +1,5 @@
 package com.github.cypher.model;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 public class Util {
 
 	// Check if sting could be a roomcollection
@@ -26,34 +18,5 @@ public class Util {
 	static boolean isUser(String s) {
 		//Todo
 		return true;
-	}
-
-	/**
-	 * Code taken from: <a href="https://community.oracle.com/thread/2238566">oracle community.</a>
-	 * Comments added.
-	 */
-	public static javafx.scene.image.Image createImage(java.awt.Image image) throws IOException {
-
-		// Make sure the image is rendered
-		if (!(image instanceof RenderedImage)) {
-			BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),
-				image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-
-			// Render the image
-			Graphics g = bufferedImage.createGraphics();
-			g.drawImage(image, 0, 0, null);
-			g.dispose();
-
-			image = bufferedImage;
-		}
-
-		// Convert image to byte array
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		ImageIO.write((RenderedImage) image, "png", out);
-		out.flush();
-
-		// Construct FX-image from byte array
-		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-		return new javafx.scene.image.Image(in);
 	}
 }
