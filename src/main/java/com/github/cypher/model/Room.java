3d3ca5de79731deafa9ad28c14db5980dd5eb5bb
hulthe <joakim@hulthe.net>
2017-05-07 18:04:30 +0200

    Add RoomItem

---
+++ src/main/java/com/github/cypher/model/Room.java
@@ -0,0 +1,53 @@
+package com.github.cypher.model;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.image.Image;
+
+public class Room {
+	private StringProperty        id;
+	private StringProperty        name;
+	private StringProperty        topic;
+	private ObjectProperty<Image> avatar;
+
+	public Room(String id, String name, String topic) {
+		this.id    = new SimpleStringProperty(id);
+		this.name  = new SimpleStringProperty(name);
+		this.topic = new SimpleStringProperty(topic);
+		this.avatar = new SimpleObjectProperty<>(null);
+	}
+
+	public String getId() {
+		return id.get();
+	}
+
+	public StringProperty idProperty() {
+		return id;
+	}
+
+	public String getName() {
+		return name.get();
+	}
+
+	public StringProperty nameProperty() {
+		return name;
+	}
+
+	public String getTopic() {
+		return topic.get();
+	}
+
+	public StringProperty topicProperty() {
+		return topic;
+	}
+
+	public Image getAvatar() {
+		return avatar.get();
+	}
+
+	public ObjectProperty<Image> avatarProperty() {
+		return avatar;
+	}
+}
