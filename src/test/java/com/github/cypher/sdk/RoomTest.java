213a1eddd0e0f25f764321984f375d8e30c6bf0a
Gurgy <Gurgy@emycia.com>
2017-05-08 21:53:27 +0200

    Add test placeholders

---
+++ src/test/java/com/github/cypher/sdk/RoomTest.java
@@ -0,0 +1,5 @@
+package com.github.cypher.sdk;
+
+public class RoomTest {
+	// Todo
+}
---
+++ src/test/java/com/github/cypher/sdk/UserTest.java
@@ -0,0 +1,5 @@
+package com.github.cypher.sdk;
+
+public class UserTest {
+	//Todo:  why does update() and update(JsonObject) do different stuff?
+}
