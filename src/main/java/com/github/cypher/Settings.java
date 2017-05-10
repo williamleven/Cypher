cb77ec9bb8fe37c5aa05f5f9b861466d79e7c3ca
Gurgy <Gurgy@emycia.com>
2017-05-10 12:39:25 +0200

    Add setting for system tray

--- src/main/java/com/github/cypher/Settings.java
+++ src/main/java/com/github/cypher/Settings.java
@@ -4,7 +4,14 @@ import java.util.Locale;
 
 public interface Settings {
 	Locale getLanguage();
+
 	void setLanguage(Locale language);
+
 	boolean getSaveSession();
+
 	void setSaveSession(boolean saveSession);
+
+	boolean getExitToSystemTray();
+
+	void setExitToSystemTray(boolean exitToSystemTray);
 }
