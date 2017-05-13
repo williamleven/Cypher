81ae4c75045c4118de01e8b548d515c5a2b4aa83
hulthe <joakim@hulthe.net>
2017-05-13 18:57:37 +0200

    Add ability to send messages from the GUI

--- src/main/java/com/github/cypher/root/roomcollection/room/chat/ChatPresenter.java
+++ src/main/java/com/github/cypher/root/roomcollection/room/chat/ChatPresenter.java
@@ -1,11 +1,18 @@
 package com.github.cypher.root.roomcollection.room.chat;
 
+import com.github.cypher.DebugLogger;
 import com.github.cypher.Settings;
 import com.github.cypher.model.Client;
+import com.github.cypher.model.Room;
+import com.github.cypher.sdk.api.RestfulHTTPException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import javax.inject.Inject;
+import java.io.IOException;
 
 public class ChatPresenter {
 
@@ -23,10 +30,38 @@ public class ChatPresenter {
 
 	@FXML
 	private void initialize() {
+		messageBox.setDisable(client.selectedRoom.getValue() == null);
+		client.selectedRoom.addListener((observable, oldValue, newValue) -> {
+			messageBox.setDisable(newValue == null);
+		});
 	}
 
 	@FXML
 	private void showRoomSettings() {
 		client.showRoomSettings.set(true);
+	}
+
+	@FXML
+	private void onMessageBoxKeyPressed(KeyEvent event) {
+		if(event.isControlDown() &&
+		   KeyCode.ENTER.equals(event.getCode()) &&
+		   !messageBox.getText().equals("")) {
+
+			Room room = client.selectedRoom.getValue();
+			if(room != null) {
+				try {
+					room.sendMessage(messageBox.getText());
+				} catch(RestfulHTTPException e) {
+					if(DebugLogger.ENABLED) {
+						DebugLogger.log("RestfulHTTPException when trying to send a message: " + e);
+					}
+				} catch(IOException e) {
+					if(DebugLogger.ENABLED) {
+						DebugLogger.log("IOException when trying to send a message: " + e);
+					}
+				}
+			}
+			messageBox.setText("");
+		}
 	}
 }
