fa2e6cb956828dc0585df951757ea6e534d459fb
NotTrulyEvil <nottrulyevil@gmail.com>
2017-05-19 14:51:19 +0200

    Switch images in General & PM collection

--- src/main/java/com/github/cypher/model/GeneralCollection.java
+++ src/main/java/com/github/cypher/model/GeneralCollection.java
@@ -7,7 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.image.Image;
 
 public class GeneralCollection implements RoomCollection {
-	private static final Image GENERAL_COLLECTION_IMAGE = new Image(GeneralCollection.class.getResourceAsStream("/images/fa-wechat-white-40.png"));
+	private static final Image GENERAL_COLLECTION_IMAGE = new Image(GeneralCollection.class.getResourceAsStream("/images/fa-users-white-40.png"));
 	private final ObjectProperty<Image> imageProperty = new SimpleObjectProperty<>(GENERAL_COLLECTION_IMAGE); // Should this maybe be generated on first request instead?
 	private final ObservableList<Room> rooms = FXCollections.synchronizedObservableList(FXCollections.observableArrayList());
 
