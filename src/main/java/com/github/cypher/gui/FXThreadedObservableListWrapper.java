6e62ca89a6267ede3d00aaa2b4906a94f3042ef4
hulthe <joakim@hulthe.net>
2017-05-21 17:39:43 +0200

    Fix FXThreadedObservableListWrapper

--- src/main/java/com/github/cypher/gui/FXThreadedObservableListWrapper.java
+++ src/main/java/com/github/cypher/gui/FXThreadedObservableListWrapper.java
@@ -1,6 +1,7 @@
 package com.github.cypher.gui;
 
 import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -10,24 +11,13 @@ import javafx.collections.ObservableList;
 public class FXThreadedObservableListWrapper<T> {
 	private final ObservableList<T> sourceList;
 	private final ObservableList<T> delegatedList;
-	private ListChangeListener<? super T> sourceListListener; // CurrentRoomCollectionListener?
+	private InvalidationListener sourceListListener; // CurrentRoomCollectionListener?
 
 	public FXThreadedObservableListWrapper(ObservableList<T> sourceList) {
 		this.sourceList = sourceList;
 		this.delegatedList = FXCollections.synchronizedObservableList(FXCollections.observableArrayList());
 		delegatedList.addAll(sourceList);
-		sourceListListener = ((ListChangeListener.Change<? extends T> change) -> {
-			Platform.runLater(() -> {
-				while (change.next()) {
-					if (change.wasAdded()) {
-						delegatedList.addAll(change.getAddedSubList());
-					}
-					if (change.wasRemoved()) {
-						delegatedList.removeAll(change.getRemoved());
-					}
-				}
-			});
-		});
+		sourceListListener = i -> Platform.runLater(() -> delegatedList.setAll(sourceList));
 		sourceList.addListener(sourceListListener);
 	}
 
