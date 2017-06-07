4581145555f0f505122af14a5e9b0e693182dbf1
Joakim Hulthe <joakim@hulthe.net>
2017-06-07 16:53:56 +0200

    Sort events by `age` not `origin_server_ts`
    This solution will ideally sorts events in the proper order.
    The drawback is that the `age` is not always verifiable by the home server.

--- src/main/java/com/github/cypher/model/Event.java
+++ src/main/java/com/github/cypher/model/Event.java
@@ -3,11 +3,13 @@ package com.github.cypher.model;
 public class Event {
 	private final String eventId;
 	private final long originServerTimeStamp;
+	private final long timeStamp;
 	private final User sender;
 
 	Event(Repository<User> repo,com.github.cypher.sdk.Event sdkEvent){
 		eventId = sdkEvent.getEventId();
 		originServerTimeStamp = sdkEvent.getOriginServerTs();
+		timeStamp = sdkEvent.getUnsignedTimeStamp();
 		sender = repo.get(sdkEvent.getSender().getId());
 	}
 
@@ -17,6 +19,10 @@ public class Event {
 
 	public long getOriginServerTimeStamp() {
 		return originServerTimeStamp;
+	}
+
+	public long getTimeStamp() {
+		return timeStamp;
 	}
 
 	public User getSender() {
